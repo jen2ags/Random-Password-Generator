173a65262e6eaf0f455eeddd789031ef692d179c
jennj <jennjenn93@gmail.com>
2022-02-25 20:36:11 -0600

    input for character length

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,47 @@
 // Assignment code here
 
+//Prompt user for password criteria after button is clicked (password length, uppercase, lowercase, numbers & special characters)
+function generatePassword() {
+  console.log("The button was clicked!!");
+
+  var characterLength = function() {
+  let characterLength=prompt("How many characters would you like in your password?");
+  let length = Number (characterLength);
+
+   if (characterLength >= 8 && characterLength <= 128) {
+    lowerCase (); //return to the next option
+    }
+    else {
+      characterLength();
+    
+    } 
+  }
+  characterLength();
+  
+  var lowerCase = function() {
+    
+    let lowerCase=confirm("Would you like to include lowercase letters?");
+
+    if (confirm == true) {
+      //move to next question
+    }
+    else {
+      //move to next question
+    }
+  }
+
+    
+
+// validate the input that the user puts in 
+
+//Password is generated
+
+
+//Display the generated password on the page
+
+
+  return "Your password was generated!";
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
